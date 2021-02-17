fdda67165c717a0ea2647dcbfe2f2698df6918a0
alex connolly <D00226250@student.dkit.ie>
2021-02-17 13:13:23 +0000

    ADDED SEPARATE VERIFY METHODS FOR LOGIN DETAILS
    Added three methods to verify caoNumber, dateOfBirth, and password
    separately in the Student class, rather than using one large method.
    These methods will be tweaked once the students are hardcoded in

--- src/main/java/com/dkit/gd2/alexconnolly/Student.java
+++ src/main/java/com/dkit/gd2/alexconnolly/Student.java
@@ -1,5 +1,7 @@
 package com.dkit.gd2.alexconnolly;
 
+import java.util.Scanner;
+
 public class Student
 {
     private int caoNumber;  // In the CAO system, cao number is unique identifier for student
@@ -7,12 +9,12 @@ public class Student
     private String password;    // min 8 characters
     private String email;
 
-    public Student copyStudent(Student student)
+    public Student copyStudent(Student studentToCopy)
     {
-        this.caoNumber = student.getCaoNumber();
-        this.dateOfBirth = student.getDateOfBirth();
-        this.password = student.getPassword();
-        this.email = student.getEmail();
+        this.caoNumber = studentToCopy.getCaoNumber();
+        this.dateOfBirth = studentToCopy.getDateOfBirth();
+        this.password = studentToCopy.getPassword();
+        this.email = studentToCopy.getEmail();
         return new Student(this.caoNumber, this.dateOfBirth, this.password, this.email);
     }
 
@@ -25,9 +27,31 @@ public class Student
         this.email = email;
     }
 
-    public boolean verifyLoginCredentials(String dateOfBirth, String password)
+    public boolean login()
     {
-        if(dateOfBirth != null && password != null)
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your CAO Number:");
+        String studentCaoNum = keyboard.nextLine();
+        System.out.println("Please enter your date of birth:");
+        String studentDob = keyboard.nextLine();
+        System.out.println("Please enter your password");
+        String studentPassword = keyboard.nextLine();
+
+        if(verifyDateOfBirth(studentDob) && verifyPassword(studentPassword) && verifyCaoNum(studentCaoNum))
+        {
+            System.out.println("You have logged in successfully");
+            return true;
+        }
+        else
+        {
+            System.out.println("One or more of your details were incorrect, please try logging in again");
+            return false;
+        }
+    }
+
+    public boolean verifyDateOfBirth(String dateOfBirth)
+    {
+        if(dateOfBirth != null)
         {
             //4 digits for year, starting with 20, 2 digits for month, firs one can't go over 1 (12 months), 2 digits for days, up to 31
             if(!dateOfBirth.matches("^20{1}\\d{2}/[0-1]{1}\\d{1}/[0-3]{1}\\d{1}"))
@@ -37,22 +61,72 @@ public class Student
             }
             else
             {
-                if (this.dateOfBirth.equals(dateOfBirth) && this.password.equals(password))
+                if (this.dateOfBirth.equals(dateOfBirth))
                 {
-                    System.out.println("Access Granted");
                     return true;
                 }
-                else if ((!this.dateOfBirth.equals(dateOfBirth) && !this.password.equals(password))
-                        || (!this.dateOfBirth.equals(dateOfBirth) || !this.password.equals(password)))
+                else if ((!this.dateOfBirth.equals(dateOfBirth)))
                 {
-                    System.out.println("One or both fields are incorrect - please enter valid credentials");
                     return false;
                 }
             }
         }
-        else if((dateOfBirth != null || password != null) || (dateOfBirth == null && password == null))
+        else if(dateOfBirth == null)
         {
-            System.out.println("Date of Birth and/or password left blank - please fill in these details");
+            return false;
+        }
+        return false;
+    }
+
+    public boolean verifyPassword(String password)
+    {
+        if(password != null)
+        {
+            if(password.length() >= 8)
+            {
+                 if (this.password.equals(password))
+                 {
+                     return true;
+                 }
+                 else if (!this.password.equals(password))
+                 {
+                     return false;
+                 }
+            }
+            else if(password.length() < 0 || password.length() > 16)
+            {
+                return false;
+            }
+        }
+        else if(password == null)
+        {
+            return false;
+        }
+        return false;
+    }
+
+    public boolean verifyCaoNum(String caoNum)
+    {
+        if(caoNum != null)
+        {
+            if(caoNum.length() == 8)
+            {
+                if(caoNum.contains("[0-9]"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if(caoNum.length() > 8 || caoNum.length() < 8)
+            {
+                return false;
+            }
+        }
+        else
+        {
             return false;
         }
         return false;
